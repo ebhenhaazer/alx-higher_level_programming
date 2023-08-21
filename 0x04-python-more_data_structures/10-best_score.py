f5c39640eadb9d4afca65a49c8c571fe83925080
ebhenhaazer <en.mwathy@gmail.com>
2023-08-21 07:26:10 -0700

    corrections

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None or a_dictionary == {}:
-        return
-    n = []
-    for new in a_dictionary:
-        n.append(new)
-    return max(n)
+    return (max(a_dictionary) if a_dictionary else None)
