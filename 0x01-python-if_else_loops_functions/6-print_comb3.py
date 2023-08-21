51af19706c1e9db12da508d3ea252941f84ff00d
ebhenhaazer <en.mwathy@gmail.com>
2023-08-20 17:07:14 -0700

    first complete python 01

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i < j:
-            if i < 8:
-                print("{}{}, ".format(i, j), end="")
-            else:
-                    print("{}{}".format(i, j))
+for i in range(10):
+    for j in range(10):
+        if(i is 8 and j is 9):
+            print("{}".format(str(i) + str(j)))
+        elif(j > i):
+            print("{}".format(str(i) + str(j)) + ", ", end="")
