59dae24f2e7c80cccd6f6c9d4daa4f0662adc10b
ebhenhaazer <en.mwathy@gmail.com>
2023-09-11 14:01:16 -0700

    10. Square #1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
-'''9-base_geometry.py'''
+"""
+This module contains a class
+with public instance and Raises
+exception when required
+"""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
-
 class Square(Rectangle):
+    """
+    class Square that inherits from rectangle
+    """
     def __init__(self, size):
-        self.integer_validator('size', size)
+        """
+        class instantiation
+        """
+        self.integer_validator("size", size)
         self.__size = size
-
-    def area(self):
-        return self.size ** 2
+        super().__init__(size, size)
