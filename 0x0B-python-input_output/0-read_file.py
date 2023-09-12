e1b9ddbbe4277009f9b6c9082fefa9a2912e080c
ebhenhaazer <en.mwathy@gmail.com>
2023-09-12 06:10:37 -0700

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,15 @@
 '''Python Function that reads a file and prints to stdout'''
 
 def read_file(filename=""):
-    with open(filename, "r") as f:
+    """ Function that reads from a file
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
+    with open(filename, 'r', encoding ='utf-8') as f:
         for line in f:
             print(line, end="")
