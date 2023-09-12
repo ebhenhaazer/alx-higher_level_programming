d672741e4dfca1fa973f860e17f6d21127f54b41
ebhenhaazer <en.mwathy@gmail.com>
2023-09-12 06:14:22 -0700

    1. Write to a file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,8 +3,17 @@
 
 
 def number_of_lines(filename=""):
+    """ Function that reads from a file and prints its number of lines
+
+    Args:
+        filename: filename
+
+    Raises
+        Exception: when the file can be opened
+
+    """
     count = 0
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         for line in f:
             count += 1
     return count
