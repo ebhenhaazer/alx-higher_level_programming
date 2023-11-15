7cfdcbd9be144308204012a259a738af26ae425a
ebhenhaazer <en.mwathy@gmail.com>
2023-11-14 23:45:23 -0800

    1. 3 languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const list = ["C is fun", "Python is cool", "Javascript is amazing"];
 let x;
 for (x = 0; x < list.length; x++) {
   console.log(list[x]);
