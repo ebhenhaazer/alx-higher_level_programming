f65368e328e59e2018c98366a61aa97a7dc2ec07
ebhenhaazer <en.mwathy@gmail.com>
2023-11-15 01:05:47 -0800

    5. An Integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-// JS to check if arguments passed cam be converted to integer
-if (isNaN(process.argv[2])) {
+const number = parseInt(process.argv[2]);
+if (Number.isNaN(number)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + parseInt(process.argv[2]));
+  console.log('My number: ' + number);
 }
