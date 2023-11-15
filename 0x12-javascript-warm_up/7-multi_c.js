4385a0ac2ee31a3a1c6d0ba6afb6ba7f714de401
ebhenhaazer <en.mwathy@gmail.com>
2023-11-15 01:37:57 -0800

    7. I love C

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
 // JS to print C is fun by number of passed in argument
-'use strict';
-let arg = process.argv[2];
-let count = 0;
-if (isNaN(arg)) {
+if (isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
-}
-while (count < arg) {
-  console.log('C is fun');
-  count++;
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('C is fun');
+  }
 }
