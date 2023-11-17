463c30b4a6cca7a7f7b83a4aa178678caf4255cc
ebhenhaazer <en.mwathy@gmail.com>
2023-11-17 05:20:45 -0800

    corrections

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -12,8 +12,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     c = db.cursor()
-    c.execute("SELECT cities.id, cities.name, states.name FROM cities\
-    JOIN states ON cities.state_id = states.id ORDER by cities.id")
+    c.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY\
+    id ASC", (argv[4],))
     for rows in c.fetchall():
         print(rows)
 
