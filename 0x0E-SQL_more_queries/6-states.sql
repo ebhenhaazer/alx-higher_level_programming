8c59fb0a522b2ffcd01cf1a3c3a08c28894d0fef
ebhenhaazer <en.mwathy@gmail.com>
2023-11-17 04:44:25 -0800

    6. States table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,10 @@
--- Script that creates a table
--- Query to create the table 'unique_id' in MySQL server
-CREATE TABLE IF NOT EXISTS unique_id (
-       id INT UNIQUE DEFAULT 1,
-       name VARCHAR(256));
+-- Create database if not exists.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- Select database to use.
+USE hbtn_0d_usa;
+-- Create user if not exists.
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+       id INT UNIQUE AUTO_INCREMENT NOT NULL,
+       name VARCHAR(256) NOT NULL,
+       PRIMARY KEY(id)
+);
